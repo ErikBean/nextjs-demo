a8b17183122bab50af01c7ccab38a4125ebabc45
Erik Bean <Erik.Bean@nike.com>
2020-09-02 15:18:23 -0700

    ccccchanges

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -8,58 +8,82 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="stage">
+        <div className="box bounce-7">
+          <div>NO LUNCH</div>
+          <div>FOR BRANDON HUNT</div>
         </div>
+      </div>
+      <main>
+        <h1 className="title"></h1>
+        <h1 className="icons">
+          <span className="eyes">🍲</span>
+          <span className="eyes">👀</span>
+        </h1>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
+        @keyframes bounce-7 {
+          0% {
+            transform: scale(1, 1) translateY(0);
+          }
+          10% {
+            transform: scale(1.1, 0.9) translateY(0);
+          }
+          30% {
+            transform: scale(0.9, 1.1) translateY(-100px);
+          }
+          50% {
+            transform: scale(1.05, 0.95) translateY(0);
+          }
+          57% {
+            transform: scale(1, 1) translateY(-7px);
+          }
+          64% {
+            transform: scale(1, 1) translateY(0);
+          }
+          100% {
+            transform: scale(1, 1) translateY(0);
+          }
+        }
+        .stage {
+          display: flex;
+          height: 330px;
+          width: 100%;
+        }
+        .box {
+          text-align: center;
+          align-self: flex-end;
+          animation-duration: 2s;
+          animation-iteration-count: infinite;
+          background-color: #f44336;
+          height: 200px;
+          margin: 0 auto 0 auto;
+          transform-origin: bottom;
+          width: 200px;
+        }
+        .bounce-7 {
+          font-size: 28px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          animation-name: bounce-7;
+          animation-timing-function: cubic-bezier(0.28, 0.84, 0.42, 1);
+        }
+
+        .title {
+          font-family: Open Sans, Helvetica, sans-serif;
+          display: online-block;
+        }
+        .icons {
+          line-height: 0;
+          transform: translateY(-150px);
+          font-size: 500px;
+        }
+        .eyes {
+          transform: scaleX(-1);
+        }
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
@@ -76,117 +100,6 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
         }
       `}</style>
 
@@ -205,5 +118,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
